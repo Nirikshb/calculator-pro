5c120d9919d8a438ee2b78048f5d7e85366ecdb4
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-06-23 11:50:59 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,52 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-function App() {
+function Calculator() {
   const [display, setDisplay] = useState('');
-  const [calcuate, setCalculate]=useState([]);
-  const [num1, setNum1] = useState("")
 
-  const handleChange = (e) => {
-    //a destructured object to easily point out the "values"
-  const {name, value} = e.target;
-  //using parseInt to convert the value from string to number
-    if(name == "num1") setNum1(parseInt(value));
+  const handleButtonClick = (value) => {
+    setDisplay(display.toString() + value);
   };
 
-  const handleClick = ()=>{
+  const handleClearClick = () => {
+    setDisplay('');
+  };
 
-    setDisplay();
-  }
+  const handleEqualClick = () => {
+    try {
+      const result = eval(display);
+      setDisplay(result.toString());
+    } catch (error) {
+      setDisplay('Error');
+    }
+  };
 
   return (
-    <>
-      <h1>Calculator</h1>
-      <div className='main'>
-        <div className='display'>{display}
-        <input type='num' placeholder='Num1' value={num1} onChange={handleChange}></input>
-        {/* Input Field */} </div>
-         
-       <p className='keypad'>
-          <button >AC</button>
-          <button >+</button>
-          <button >-</button>
-          <button >*</button>
-          <button >7</button>
-          <button >8</button>
-          <button >9</button>
-          <button >4</button>
-          <button >5</button>
-          <button >6</button>
-          <button >1</button>
-          <button >2</button>
-          <button >3</button>
-          <button >0</button>
-          <button >.</button>
-          <button >=</button>    
-          </p>
+    <div className="calculator">
+      <h2> Calculaor</h2>
+      <div className="display">{display}</div>
+      <div className="keypad">
+        <button onClick={() => handleButtonClick("7")}>7</button>
+        <button onClick={() => handleButtonClick("8")}>8</button>
+        <button onClick={() => handleButtonClick("9")}>9</button>
+        <button onClick={() => handleButtonClick("+")}>+</button>
+        <button onClick={() => handleButtonClick("4")}>4</button>
+        <button onClick={() => handleButtonClick("5")}>5</button>
+        <button onClick={() => handleButtonClick("6")}>6</button>
+        <button onClick={() => handleButtonClick("-")}>-</button>
+        <button onClick={() => handleButtonClick("1")}>1</button>
+        <button onClick={() => handleButtonClick("2")}>2</button>
+        <button onClick={() => handleButtonClick("3")}>3</button>
+        <button onClick={() => handleButtonClick("*")}>*</button>
+        <button onClick={() => handleButtonClick("0")}>0</button>
+        <button onClick={() => handleButtonClick(".")}>.</button>
+        <button onClick={handleEqualClick}>=</button>
+        <button onClick={handleClearClick}>AC</button>
+        <button onClick={() => handleButtonClick("/")}>/</button>
+        <button>Switch</button>
       </div>
-     
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default Calculator;
