cbf43e18b1e5a5d86a574d50f67f9c6d2d7b5d1d
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-06-29 14:30:16 +0530

    Initial commit

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 
 const Calculator = () => {
   const [history, setHistory] = useState([]);
@@ -39,24 +40,38 @@ const Calculator = () => {
   return (
     <div className="calculator">
       <h2>Calculator</h2>
-      <div className="display">{display}</div>
+      <div className="display">{display}
+      </div>
+       
+
       <div className="keypad">
-        <button onClick={() => handleButtonClick('7')}>7</button>
-        <button onClick={() => handleButtonClick('8')}>8</button>
-        <button onClick={() => handleButtonClick('9')}>9</button>
-        <button onClick={() => handleButtonClick('+')}>+</button>
-        <button onClick={() => handleButtonClick('4')}>4</button>
-        <button onClick={() => handleButtonClick('5')}>5</button>
-        <button onClick={() => handleButtonClick('6')}>6</button>
-        <button onClick={() => handleButtonClick('-')}>-</button>
-        <button onClick={() => handleButtonClick('1')}>1</button>
-        <button onClick={() => handleButtonClick('2')}>2</button>
-        <button onClick={() => handleButtonClick('3')}>3</button>
-        <button onClick={() => handleButtonClick('*')}>*</button>
-        <button onClick={() => handleButtonClick('0')}>0</button>
-        <button onClick={() => handleButtonClick('.')}>.</button>
-        <button onClick={handleEqualClick}>=</button>
-        <button onClick={handleClearClick}>AC</button>
+        <div className="keypad-row">
+          <button onClick={() => handleButtonClick('7')}>7</button>
+          <button onClick={() => handleButtonClick('8')}>8</button>
+          <button onClick={() => handleButtonClick('9')}>9</button>
+          <button onClick={() => handleButtonClick('/')}>/</button>
+        </div>
+        <div className="keypad-row">
+          <button onClick={() => handleButtonClick('4')}>4</button>
+          <button onClick={() => handleButtonClick('5')}>5</button>
+          <button onClick={() => handleButtonClick('6')}>6</button>
+          <button onClick={() => handleButtonClick('*')}>*</button>
+        </div>
+        <div className="keypad-row">
+          <button onClick={() => handleButtonClick('1')}>1</button>
+          <button onClick={() => handleButtonClick('2')}>2</button>
+          <button onClick={() => handleButtonClick('3')}>3</button>
+          <button onClick={() => handleButtonClick('-')}>-</button>
+        </div>
+        <div className="keypad-row">
+          <button onClick={() => handleButtonClick('0')}>0</button>
+          <button onClick={() => handleButtonClick('.')}>.</button>
+          <button onClick={handleEqualClick}>=</button>
+          <button onClick={() => handleButtonClick('+')}>+</button>
+        </div>
+        <div className="keypad-row">
+          <button onClick={handleClearClick}>AC</button>
+        </div>
       </div>
       <button onClick={handleHistoryClick}>
         {showHistory ? 'Hide History' : 'Show History'}
--- src/spilt.jsx
+++ src/spilt.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
 const SplitExpenseCalculator = () => {
   const [totalAmount, setTotalAmount] = useState('');
   const [numberOfFriends, setNumberOfFriends] = useState('');
   const [result, setResult] = useState(null);
+  const [expenses, setExpenses] = useState([]);
+  const [splitExpenses, setSplitExpenses] = useState([]);
 
   const handleTotalAmountChange = (e) => {
     setTotalAmount(e.target.value);
@@ -11,6 +14,23 @@ const SplitExpenseCalculator = () => {
 
   const handleNumberOfFriendsChange = (e) => {
     setNumberOfFriends(e.target.value);
+  };
+
+  const handleExpenseAdd = () => {
+    if (totalAmount) {
+      const newExpense = {
+        amount: totalAmount,
+        description: '',
+      };
+      setExpenses((prevExpenses) => [...prevExpenses, newExpense]);
+      setTotalAmount('');
+    }
+  };
+
+  const handleExpenseChange = (index, e) => {
+    const updatedExpenses = [...expenses];
+    updatedExpenses[index].description = e.target.value;
+    setExpenses(updatedExpenses);
   };
 
   const calculateSplitExpense = () => {
@@ -22,18 +42,51 @@ const SplitExpenseCalculator = () => {
     }
   };
 
+  const handleSplitExpenses = () => {
+    const totalExpense = expenses.reduce(
+      (accumulator, expense) => accumulator + Number(expense.amount),
+      0
+    );
+    const amountPerFriend = (totalExpense / numberOfFriends).toFixed(2);
+    const splitExpenses = expenses.map((expense) => ({
+      ...expense,
+      amount: amountPerFriend,
+    }));
+    setSplitExpenses(splitExpenses);
+  };
+
+  const calculateTotalExpense = () => {
+    const total = expenses.reduce(
+      (accumulator, expense) => accumulator + Number(expense.amount),
+      0
+    );
+    return total.toFixed(2);
+  };
+
   return (
-    <div className="split-expense-calculator">
+    <motion.div
+      className="split-expense-calculator"
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '100vh',
+      }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
       <h2>Split Expense Calculator</h2>
-      <div>
+      <div className="input-section">
         <label>Total Amount:</label>
         <input
           type="number"
           value={totalAmount}
           onChange={handleTotalAmountChange}
         />
+        <button onClick={handleExpenseAdd}>Add Expense</button>
       </div>
-      <div>
+      <div className="input-section">
         <label>Number of Friends:</label>
         <input
           type="number"
@@ -43,7 +96,45 @@ const SplitExpenseCalculator = () => {
       </div>
       <button onClick={calculateSplitExpense}>Calculate</button>
       {result && <p>Amount per friend: ${result}</p>}
-    </div>
+
+      <div className="expense-list">
+        {expenses.map((expense, index) => (
+          <div key={index} className="expense-box">
+            <input
+              type="text"
+              value={expense.amount}
+              readOnly
+              className="expense-amount"
+            />
+            <input
+              type="text"
+              value={expense.description}
+              onChange={(e) => handleExpenseChange(index, e)}
+              className="expense-description"
+            />
+          </div>
+        ))}
+      </div>
+
+      {expenses.length > 0 && (
+        <div className="split-button">
+          <button onClick={handleSplitExpenses}>Split Expenses</button>
+        </div>
+      )}
+
+      {splitExpenses.length > 0 && (
+        <div className="split-expenses">
+          <h3>Split Expenses</h3>
+          {splitExpenses.map((expense, index) => (
+            <div key={index} className="split-expense">
+              <span>{expense.amount}</span>
+              <span>{expense.description}</span>
+            </div>
+          ))}
+          <p>Total Expense: ${calculateTotalExpense()}</p>
+        </div>
+      )}
+    </motion.div>
   );
 };
 
